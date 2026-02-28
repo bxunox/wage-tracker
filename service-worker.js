ba9253a1b7ff3526bbbc9c8fc9fb1ac89e4d1888
bxunox <brunooo.novak@gmail.com>
2026-02-28 01:08:30 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,35 @@
-const CACHE_NAME = "wage-tracker-v1";
+const CACHE_NAME = "wage-tracker-v2"; // increment this whenever you update files
 
 self.addEventListener("install", e => {
-e.waitUntil(
-caches.open(CACHE_NAME).then(cache =>
-cache.addAll([
-"./",
-"index.html",
-"style.css",
-"script.js",
-"manifest.json",
-"icon-192.png",
-"icon-512.png"
-])
-)
-);
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "./",
+        "index.html",
+        "style.css",
+        "script.js",
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
+      ])
+    )
+  );
+  self.skipWaiting(); // activate new SW immediately
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim(); // take control of pages immediately
 });
 
 self.addEventListener("fetch", e => {
-e.respondWith(
-caches.match(e.request).then(response => response || fetch(e.request))
-);
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
